82e9ccb3c91c441de057a8f16554d144416a3873
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-20 15:10:34 +0800

    set up individual ex page for both reading and writing

---
+++ pages/writings/[slug].js
@@ -0,0 +1,26 @@
+import Layout from '../../components/Layout';
+import { API_URL } from '@/config/index';
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
+
+export async function getServerSideProps({ query: { slug } }) {
+	const res = await fetch(`${API_URL}/writings?slug=${slug}`);
+	const ex = await res.json();
+	return {
+		props: {
+			ex: ex[0]
+		}
+	};
+}
+
+export default function WritingExPage({ ex }) {
+	console.log(ex);
+	return (
+		<Layout title='Ex'>
+			<h1>Writing Ex Page</h1>
+			<Link href='/readings'>
+				<Button variant='light'>Back</Button>
+			</Link>
+		</Layout>
+	);
+}
