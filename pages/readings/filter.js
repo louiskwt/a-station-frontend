d375c04728f2e29e833425b8a22e680cdf77f080
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-22 12:01:57 +0800

    displaying filtered results

--- pages/readings/filter.js
+++ pages/readings/filter.js
@@ -3,6 +3,8 @@ import { API_URL } from '@/config//index';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import qs from 'qs';
+import Container from 'react-bootstrap/Container';
+import ExerciseCard from '@/components/ExerciseCard';
 
 // Fetching the filtered data
 export async function getServerSideProps({ query: { tag } }) {
@@ -27,10 +29,23 @@ export default function ReadingFilterPage({ ex }) {
 	const router = useRouter();
 	return (
 		<Layout>
-			<Link href='/readings'>
-				<a className='mt-5 btn btn-warning'>Clear Filter Result</a>
-			</Link>
-			<h3>Filter result for: {router.query.tag}</h3>
+			<Container>
+				<h3 className='mt-5'>Filter result for: {router.query.tag}</h3>
+
+				{ex.map((exercise) => (
+					<ExerciseCard
+						exercise={exercise}
+						key={exercise.id}
+						className='w-100'
+					/>
+				))}
+
+				<br />
+
+				<Link href='/readings'>
+					<a className='mt-5 btn btn-warning'>Clear Filter Result</a>
+				</Link>
+			</Container>
 		</Layout>
 	);
 }
--- pages/writings/filter.js
+++ pages/writings/filter.js
@@ -3,6 +3,8 @@ import { API_URL } from '@/config//index';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import qs from 'qs';
+import Container from 'react-bootstrap/Container';
+import ExerciseCard from '@/components/ExerciseCard';
 
 // Fetching the filtered data
 export async function getServerSideProps({ query: { tag } }) {
@@ -25,10 +27,21 @@ export default function WritingFilterPage({ ex }) {
 	const router = useRouter();
 	return (
 		<Layout>
-			<Link href='/writings'>
-				<a className='mt-5 btn btn-warning'>Clear Filter Result</a>
-			</Link>
-			<h3>Filter result for: {router.query.tag}</h3>
+			<Container>
+				<h3 className='mt-5'>Filter result for: {router.query.tag}</h3>
+				{ex.map((exercise) => (
+					<ExerciseCard
+						exercise={exercise}
+						key={exercise.id}
+						className='w-100'
+					/>
+				))}
+				<br />
+
+				<Link href='/writings'>
+					<a className='mt-5 btn btn-warning'>Clear Filter Result</a>
+				</Link>
+			</Container>
 		</Layout>
 	);
 }
