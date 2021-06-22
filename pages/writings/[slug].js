2a563b8b776da26e2131612d17ac54d97367a703
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-22 10:52:14 +0800

    writing ex layout

--- pages/writings/[slug].js
+++ pages/writings/[slug].js
@@ -2,6 +2,10 @@ import Layout from '../../components/Layout';
 import { API_URL } from '@/config/index';
 import Link from 'next/link';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Image from 'next/image';
+import SAPanel from '@/components/SAPanel';
+import MCPanel from '@/components/MCPanel';
 
 export async function getServerSideProps({ query: { slug } }) {
 	const res = await fetch(`${API_URL}/writings?slug=${slug}`);
@@ -17,7 +21,23 @@ export default function WritingExPage({ ex }) {
 	console.log(ex);
 	return (
 		<Layout title={ex.title}>
-			<h1>Writing Ex Page</h1>
+			<Container>
+				<h2 className='mt-3'>{ex.title}</h2>
+				<br />
+				<br />
+				<div className='text-center'>
+					<Image
+						src={ex.cover.formats.small.url}
+						alt='cover'
+						width={ex.cover.formats.small.width}
+						height={ex.cover.formats.small.height}
+					/>
+				</div>
+				<br />
+				{ex.mc ? <MCPanel /> : <SAPanel />}
+			</Container>
+			<br />
+			<br />
 			<Link href='/writings'>
 				<Button variant='light'>Back</Button>
 			</Link>
