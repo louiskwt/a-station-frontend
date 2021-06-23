93ebdf1fbe1656a32a648b3d1995bfce5fac6378
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-23 12:00:29 +0800

    getting the selected options from users in MCPannel

--- components/MCPanel.js
+++ components/MCPanel.js
@@ -4,9 +4,28 @@ import Form from 'react-bootstrap/Form';
 export default function AnswerPanel({ questions }) {
 	const questionData = questions.questions;
 	const instruction = questions.instruction;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const total = questions.questions.length;
+
+		const responseArr = [];
+		// Getting the selected options from users
+		for (let i = 0; i < total; i++) {
+			const target = `e.target.q${i + 1}`;
+			target = eval(target);
+			const optionArr = Array.from(target);
+			for (let j = 0; j < optionArr.length; j++) {
+				if (optionArr[j].checked === true) {
+					responseArr.push(optionArr[j].id);
+				}
+			}
+		}
+		console.log(responseArr);
+	};
 	return (
 		<div className='mt-4 col-10 offset-1'>
-			<Form>
+			<Form onSubmit={handleSubmit}>
 				<h5>{instruction}</h5>
 				{questionData.map((data, index) => (
 					<Form.Group key={data.question}>
@@ -17,10 +36,11 @@ export default function AnswerPanel({ questions }) {
 						{data.options.map((option) => (
 							<Form.Check
 								inline
-								name={option}
+								name={`q${index + 1}`}
 								type='radio'
 								label={option}
 								key={option}
+								id={option}
 							/>
 						))}
 					</Form.Group>
