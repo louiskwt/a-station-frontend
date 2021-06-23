e98d3d8b2f7407706adc410c01d144eb39fac2ae
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-23 10:56:17 +0800

    fixed unique key props issue in SAPanel

--- components/SAPanel.js
+++ components/SAPanel.js
@@ -6,15 +6,19 @@ import Col from 'react-bootstrap/Col';
 export default function SAPanel({ questions }) {
 	const questionData = questions.questions[0];
 	console.log(questionData.options);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log('submitted');
+	};
 
 	return (
 		<div className='mt-4 col-10 offset-1'>
-			<Form>
+			<Form onSubmit={handleSubmit}>
 				<h5>{questionData.instruction}</h5>
 				<br />
 				<br />
 				{questionData.options.map((option, index) => (
-					<Form.Group as={Row}>
+					<Form.Group as={Row} key={option}>
 						<Form.Label column sm='6'>
 							Q{index + 1}: {option}
 						</Form.Label>
