d5d72129fcb0606863ffb9b99e68bc435ef50518
Ka Wing Tsang <kawingt2@gmail.com>
2021-06-28 12:15:36 +0800

    dashboard page

---
+++ pages/dashboard.js
@@ -0,0 +1,35 @@
+import Layout from '@/components/Layout';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import AuthContext from '@/context/AuthContext';
+import { useContext } from 'react';
+
+export default function DashboardPage() {
+	const { user } = useContext(AuthContext);
+	const userInfo = [];
+
+	return (
+		<Layout>
+			<Container>
+				<h1 className='text-center mt-5'>{user.username}</h1>
+				<h2>用户資料</h2>
+				<Table className='mt-5'>
+					<tbody>
+						<tr>
+							<td>Email: </td>
+							<td>{user.email}</td>
+						</tr>
+						<tr>
+							<td>Membership: </td>
+							<td>Free</td>
+						</tr>
+						<tr>
+							<td>註冊日期 </td>
+							<td>{user.created_at.slice(0, 10)}</td>
+						</tr>
+					</tbody>
+				</Table>
+			</Container>
+		</Layout>
+	);
+}
