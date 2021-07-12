329d2d7021bd58118257adf5284bf7858c5f5c12
Ka Wing Tsang <kawingt2@gmail.com>
2021-07-12 11:55:41 +0800

    updated forgot password page

--- pages/forgotpassword.js
+++ pages/forgotpassword.js
@@ -1,72 +1,28 @@
 import Layout from '@/components/Layout';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Spinner from 'react-bootstrap/Spinner';
-import Link from 'next/link';
-import AuthContext from '@/context/AuthContext';
-import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useState, useContext } from 'react';
+import Link from 'next/link';
+import { FaWhatsapp } from 'react-icons/fa';
 
 export default function ForgotPasswordPage() {
-	// ContextAPI state and function
-	const { login, loading, setLoading } = useContext(AuthContext);
-	// Login State
-	const [email, setEmail] = useState('');
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setLoading(true);
-		if (e.target.email.value === '') {
-			toast.error('請先輸入電郵', {
-				position: 'top-center'
-			});
-			setLoading(false);
-			return;
-		}
-	};
-
 	return (
 		<Layout>
 			<Container>
-				<h2 className='text-center mt-5'>忘記密碼</h2>
-				<p className='text-center mt-3'>
-					請輸入你註冊時使用的電郵去重設密碼～
-				</p>
-				<Form onSubmit={handleSubmit}>
-					<Form.Group>
-						<Form.Label>Email</Form.Label>
-
-						<Form.Control
-							type='email'
-							placeholder='Email'
-							name='email'
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</Form.Group>
-
-					<Button
-						variant='success'
-						className='mt-5'
-						type='submit'
-						block
-					>
-						{loading ? (
-							<Spinner
-								as='span'
-								animation='border'
-								size='sm'
-								role='status'
-								aria-hidden='true'
-								variant='light'
-							/>
-						) : (
-							'重設密碼'
-						)}
-					</Button>
-				</Form>
+				<h2 className='text-center mt-5'>忘記密碼?</h2>
+				<h4 className='text-center mt-5'>
+					請email: kawingt2@gmail.com
+				</h4>
+				<h4 className='text-center mt-3'>或</h4>
+				<h4 className='text-center mt-3'>
+					通過WhatsApp:{' '}
+					<Link href='https://wa.me/85263520220?text=我想重設密碼%20thanks!'>
+						<a>
+							<FaWhatsapp size={36} style={{ color: 'green' }} />
+						</a>
+					</Link>{' '}
+					去聯絡我
+				</h4>
+				<h4 className='text-center mt-5'>我會盡快幫你重設密碼</h4>
 			</Container>
 		</Layout>
 	);
