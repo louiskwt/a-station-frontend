d8beb6d336b1e46abf876d48b75e9e58bb467697
Ka Wing Tsang <kawingt2@gmail.com>
2021-07-18 13:05:41 +0800

    conditionally rendered ranking table in modal

--- components/Ranking.js
+++ components/Ranking.js
@@ -10,28 +10,32 @@ export default function Ranking({ show, rankingData, handleClose }) {
 				<Modal.Title>排行榜</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
-				<Table striped bordered hover>
-					<thead>
-						<tr>
-							<th>Rank</th>
-							<th>Username</th>
-							<th>Time</th>
-							<th>Score</th>
-						</tr>
-					</thead>
-					<tbody>
-						{rankingData.map((data, index) => (
-							<tr key={index}>
-								<td>{index + 1}</td>
-								<td>{data.user.username}</td>
-								<td>{data.time}</td>
-								<td>
-									{data.score} / {data.total}
-								</td>
+				{rankingData.length > 0 ? (
+					<Table striped bordered hover>
+						<thead>
+							<tr>
+								<th>Rank</th>
+								<th>Username</th>
+								<th>Time</th>
+								<th>Score</th>
 							</tr>
-						))}
-					</tbody>
-				</Table>
+						</thead>
+						<tbody>
+							{rankingData.map((data, index) => (
+								<tr key={index}>
+									<td>{index + 1}</td>
+									<td>{data.user.username}</td>
+									<td>{data.time}</td>
+									<td>
+										{data.score} / {data.total}
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</Table>
+				) : (
+					'還未有排名，馬上儲存成績去成為第一位啦！'
+				)}
 			</Modal.Body>
 		</Modal>
 	);
