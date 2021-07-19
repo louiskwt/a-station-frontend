6c3fe52443edd77c695e29b9c40376c82ea565aa
Ka Wing Tsang <kawingt2@gmail.com>
2021-07-19 10:12:55 +0800

    fixed RecordTable error

--- components/RecordTable.js
+++ components/RecordTable.js
@@ -4,14 +4,16 @@ export default function RecordTable({ record }) {
 	return (
 		<Table bordered hover className='mt-5'>
 			<thead>
-				<td>練習</td>
-				<td>成績</td>
-				<td>時間</td>
-				<td>日期</td>
+				<tr>
+					<td>練習</td>
+					<td>成績</td>
+					<td>時間</td>
+					<td>日期</td>
+				</tr>
 			</thead>
 			<tbody>
-				{record.map((data) => (
-					<tr>
+				{record.map((data, index) => (
+					<tr key={index}>
 						<td>{data.title}</td>
 						<td>
 							{data.score} / {data.total}
