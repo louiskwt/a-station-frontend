eedd5691162a705dfd4ebbd610c15010e155af3f
Ka Wing Tsang <kawingt2@gmail.com>
2021-08-02 09:59:38 +0800

    Changed Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,20 @@ import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { AuthProvider } from '@/context/AuthContext';
 import { ScoreProvider } from '@/context/ScoreContext';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<AuthProvider>
-			<ScoreProvider>
-				<Component {...pageProps} />
-			</ScoreProvider>
-		</AuthProvider>
+		<>
+			<Head>
+				<link rel='icon' href='/favicon.png' />
+			</Head>
+			<AuthProvider>
+				<ScoreProvider>
+					<Component {...pageProps} />
+				</ScoreProvider>
+			</AuthProvider>
+		</>
 	);
 }
 
